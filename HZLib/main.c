cba603905dd90750b98ca4906b9f41d2161f2e0d
Materone <materone@gmail.com>
2013-11-16 20:49:53 +0000

    Initial Commit

---
+++ HZLib/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HZLib
+//
+//  Created by tony on 13-11-16.
+//  Copyright (c) 2013年 tony. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
